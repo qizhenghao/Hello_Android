7751188dc8085e9aa6feed6a4626bd2f02c4a7d1
qizhenghao <qizhenghao258@163.com>
2017-03-12 10:54:06 +0800

    java 8 new

---
+++ app/src/main/java/com/bruce/android/knowledge/Java8.java
@@ -0,0 +1,11 @@
+package com.bruce.android.knowledge;
+
+/**
+ * Created by qizhenghao on 17/3/11.
+ */
+public class Java8 {
+
+    public static void hhh() {
+//        Runnable runnable = () -> {};
+    }
+}
