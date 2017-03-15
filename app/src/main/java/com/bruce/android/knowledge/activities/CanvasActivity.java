af435ad9583c4902da78f086c78113a71b2bec60
qizhenghao <qizhenghao258@163.com>
2017-03-15 15:47:32 +0800

    画椭圆

--- app/src/main/java/com/bruce/android/knowledge/activities/CanvasActivity.java
+++ app/src/main/java/com/bruce/android/knowledge/activities/CanvasActivity.java
@@ -7,9 +7,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 
 public class CanvasActivity extends Activity {
+    private DisplayMetrics displayMetrics;
+
     /**
      * 画一个几何图形
      * hades
@@ -20,6 +24,10 @@ public class CanvasActivity extends Activity {
         super.onCreate(savedInstanceState);
         MyView myView = new MyView(this);
         setContentView(myView);
+
+        displayMetrics = new DisplayMetrics();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getMetrics(displayMetrics);
     }
 
     public class MyView extends View {
@@ -55,9 +63,78 @@ public class CanvasActivity extends Activity {
             canvas.drawRect(20, 70, 70, 120, paint);
             // 画一个长方形
             canvas.drawRect(20, 170, 90, 230, paint);
-            // 画一个椭圆
-            RectF re = new RectF(20, 250, 100, 400);
-            canvas.drawOval(re, paint);
+
+//            int padding = 20;
+//            int r = displayMetrics.widthPixels - padding * 2;
+//            int left = displayMetrics.widthPixels/2 - 100;
+//            int right = displayMetrics.widthPixels/2 + 100;
+//            int leftMin = padding;
+//            int top = 200;
+//            int bottom = r + top + (left-leftMin)/20*20;
+//            int everyPadding = 20;
+//            while (left > leftMin) {
+//                // 画一个椭圆
+//                RectF re = new RectF(left, top, right, bottom);
+//                canvas.drawOval(re, paint);
+//                left -= everyPadding;
+//                right += everyPadding;
+//                top += everyPadding;
+//                bottom -= everyPadding;
+//            }
+
+            paint.setColor(Color.BLACK);
+            float padding = 100;
+            float r = (displayMetrics.widthPixels - padding * 2)/2;
+            float left = padding;
+            float leftMax = displayMetrics.widthPixels/2 - 100;
+            float changed = leftMax - left;
+            float top = 720;
+            float bottom = 2*r + top + changed;
+            top -= changed;
+            float leftMin = left;
+            left = leftMax;
+            float right = displayMetrics.widthPixels/2 + 100;
+            float everyPadding = 20;
+            while (left > leftMin) {
+                paint.setAlpha((int) ((1.1-(left-leftMin)*1.0f/changed)*255));
+                RectF re = new RectF(left, top, right, bottom);
+                // 画一个椭圆
+                canvas.drawOval(re, paint);
+                left -= everyPadding;
+                right += everyPadding;
+                top += everyPadding;
+                bottom -= everyPadding;
+            }
+
+
+//            paint.setColor(Color.BLACK);
+//            float centerPointLeft = displayMetrics.widthPixels/2;
+//            float centerPointTop = displayMetrics.heightPixels/2;
+//            float padding = 100;
+//            float fixedMin = 330;
+//            float r = (displayMetrics.widthPixels - padding * 2)/2;
+//            float s = r * r;
+//
+//            float left = centerPointLeft - fixedMin;
+//            float right = centerPointLeft + fixedMin;
+//            float a = (right - left) / 2;
+//            float b = s / a;
+//            float top = centerPointTop - b;
+//            float bottom = centerPointTop + b;
+//
+//            float everyPadding = 10;
+//            while (a < b) {
+//                paint.setAlpha((int) ((r - (left - padding))/r*255));
+//                RectF re = new RectF(left, top, right, bottom);
+//                // 画一个椭圆
+//                canvas.drawOval(re, paint);
+//                left -= everyPadding;
+//                right += everyPadding;
+//                a = (right - left) / 2;
+//                b = s / a;
+//                top = centerPointTop - b;
+//                bottom = centerPointTop + b;
+//            }
 
 //            /**
 //             * 定义矩形为实心
